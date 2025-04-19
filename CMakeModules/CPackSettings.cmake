e3090cbabddbcec5996c25bd567c9da3af6ea687
JafarAbdi <jafar.uruc@gmail.com>
2025-04-19 14:19:58 +0000

    Rebase from 'release/jazzy/ompl'

--- CMakeModules/CPackSettings.cmake
+++ CMakeModules/CPackSettings.cmake
@@ -3,16 +3,13 @@ set(CPACK_PACKAGE_VENDOR "Rice University")
 set(CPACK_PACKAGE_CONTACT "Mark Moll <mmoll@rice.edu>")
 
 # component list
-set(CPACK_COMPONENTS_ALL ompl python morse)
+set(CPACK_COMPONENTS_ALL ompl python)
 # display names for components
 set(CPACK_COMPONENT_OMPL_DISPLAY_NAME "OMPL library, headers, and demos")
 set(CPACK_COMPONENT_PYTHON_DISPLAY_NAME "Python bindings")
-set(CPACK_COMPONENT_MORSE_DISPLAY_NAME "Blender/MORSE plugin")
 # descriptions of components
-set(CPACK_COMPONENT_MORSE_DESCRIPTION "The Blender/MORSE plugin allows one to plan paths using the MORSE robot simulator. MORSE is built on top of Blender and uses its built-in physics engine to compute physically realistic motions.")
 # intercomponent dependencies
 set(CPACK_COMPONENT_PYTHON_DEPENDS ompl)
-set(CPACK_COMPONENT_MORSE_DEPENDS python)
 # core library is required
 set(CPACK_COMPONENT_OMPL_REQUIRED ON)
 
@@ -48,7 +45,7 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
         OUTPUT_VARIABLE UBUNTU_RELEASE
         OUTPUT_STRIP_TRAILING_WHITESPACE)
     set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}_${PROJECT_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}-Ubuntu${UBUNTU_RELEASE}")
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "python${PYTHON_VERSION}, libboost-serialization-dev, libboost-filesystem-dev, libboost-system-dev, libboost-program-options-dev, libboost-test-dev, libode-dev, libtriangle-dev")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "python${PYTHON_VERSION}, libboost-serialization-dev, libboost-filesystem-dev, libboost-system-dev, libboost-program-options-dev, libboost-test-dev, libtriangle-dev")
 endif()
 
 if(WIN32)
